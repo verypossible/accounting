7b98f39107dbf04a29be927073b1163f47963c93
Bryan Enders <endersstocker@users.noreply.github.com>
2017-02-08 10:29:18 -0500

    Hotfix: Adapter config
    Why:
    
    * Adapter config changes weren’t picked up after Accounting was
      compiled.
    
    How:
    
    * Fetch the `:adapter` config var when it’s used instead of during
      compilation.

--- lib/accounting/application.ex
+++ lib/accounting/application.ex
@@ -1,17 +1,19 @@
 defmodule Accounting.Application do
   use Application
 
-  @moduledoc false
+  alias Accounting.TestAdapter
 
-  @adapter Application.get_env(:accounting, :adapter, Accounting.TestAdapter)
+  @moduledoc false
 
   ## Callbacks
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    Supervisor.start_link [worker(@adapter, [])],
+    Supervisor.start_link [worker(adapter(), [])],
       strategy: :one_for_one,
       name: Accounting.Supervisor
   end
+
+  defp adapter, do: Application.get_env(:accounting, :adapter, TestAdapter)
 end
--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Accounting.Mixfile do
       description: "Accounting.",
       elixir: "~> 1.4",
       package: package(),
-      version: "0.1.1",
+      version: "0.1.2",
       start_permanent: Mix.env === :prod,
     ]
   end
