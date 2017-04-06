8aefe31164dcf70cadbc5693b923f7c8ca03699a
Bryan Enders <endersstocker@users.noreply.github.com>
2017-04-06 16:18:17 -0400

    Fix: Adapter Behaviour
    Why
    ---
    
    The adapter `create_account` now has an arity of 3.
    
    How
    ---
    
    Update `Accounting.Adapter` behaviour.

--- lib/accounting/adapter.ex
+++ lib/accounting/adapter.ex
@@ -1,7 +1,7 @@
 defmodule Accounting.Adapter do
   @callback start_link() :: Supervisor.on_start
   @callback register_categories([atom], timeout) :: :ok | {:error, any}
-  @callback create_account(String.t, timeout) :: :ok | {:error, any}
+  @callback create_account(String.t, String.t, timeout) :: :ok | {:error, any}
   @callback receive_money(String.t, Date.t, [Accounting.LineItem.t], timeout) :: :ok | {:error, any}
   @callback spend_money(String.t, Date.t, [Accounting.LineItem.t], timeout) :: :ok | {:error, any}
   @callback fetch_account_transactions(String.t, timeout) :: {:ok, [Accounting.AccountTransaction.t]} | {:error, any}
