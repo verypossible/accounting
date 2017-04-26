1c3827fa97d7317ef8c777d70731e9568dc098d4
Bryan Enders <endersstocker@users.noreply.github.com>
2017-04-26 14:15:31 -0400

    Enhancement: Limit Xero API fetch frequency
    Why
    ---
    
    Xero rate-limits API calls to 60 per minute.
    
    How
    ---
    
    Fetch new transactions, at most, once per second.
    
    Also
    ----
    
    Add and improve typespecs.

--- lib/accounting/adapter.ex
+++ lib/accounting/adapter.ex
@@ -1,8 +1,8 @@
 defmodule Accounting.Adapter do
   @callback start_link() :: Supervisor.on_start
-  @callback register_categories([atom], timeout) :: :ok | {:error, any}
-  @callback create_account(String.t, String.t, timeout) :: :ok | {:error, any}
-  @callback receive_money(String.t, Date.t, [Accounting.LineItem.t], timeout) :: :ok | {:error, any}
-  @callback spend_money(String.t, Date.t, [Accounting.LineItem.t], timeout) :: :ok | {:error, any}
-  @callback fetch_account_transactions(String.t, timeout) :: {:ok, [Accounting.AccountTransaction.t]} | {:error, any}
+  @callback register_categories([atom], timeout) :: :ok | {:error, term}
+  @callback create_account(String.t, String.t, timeout) :: :ok | {:error, term}
+  @callback receive_money(String.t, Date.t, [Accounting.LineItem.t], timeout) :: :ok | {:error, term}
+  @callback spend_money(String.t, Date.t, [Accounting.LineItem.t], timeout) :: :ok | {:error, term}
+  @callback fetch_account_transactions(String.t, timeout) :: {:ok, [Accounting.AccountTransaction.t]} | {:error, term}
 end
