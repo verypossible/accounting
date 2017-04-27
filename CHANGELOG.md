682b636237b71287434f55e01a63b8aa309f28d1
Bryan Enders <endersstocker@users.noreply.github.com>
2017-04-27 05:18:56 -0400

    Remove adapter auto-start
    Why
    ---
    
    You had to configure the application environment for this dependency.
    This was heavy-handed and is no longer considered a best-practice in
    Elixir.
    
    How
    ---
    
    * Move process start to `Accounting.start_link/1` with config options.
    * Add opts to `start_link` adapter callback.
    * Bump version to v0.5.0.
    * Update CHANGELOG.
    
    Side effects
    ------------
    
    * Third-party adapters will need to be updated to support `start_link`
      opts.
    * Dependent apps will need to start Accounting.
    * Dependent app Accounting config will need to be moved from
      application environment to `Accounting.start_link/1`.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,16 @@
 # Changelog
 
-## v0.4.2 (2012-04-26)
+## v0.5.0 (2017-04-27)
+
+Accounting no longer starts its adapter processes automatically. You can now
+start them by calling `Accounting.start_link/1` with configuration options,
+including the adapter you wish to use.
+
+In previous Accounting versions, you had to configure the application
+environment for this dependency. This was heavy-handed and is no longer
+considered a best-practice in Elixir.
+
+## v0.4.2 (2017-04-26)
 
 ### 1. Enhancements
 
--- lib/accounting/adapter.ex
+++ lib/accounting/adapter.ex
@@ -1,8 +1,8 @@
 defmodule Accounting.Adapter do
-  @callback start_link() :: Supervisor.on_start
-  @callback register_categories([atom], timeout) :: :ok | {:error, term}
   @callback create_account(String.t, String.t, timeout) :: :ok | {:error, term}
-  @callback receive_money(String.t, Date.t, [Accounting.LineItem.t], timeout) :: :ok | {:error, term}
-  @callback spend_money(String.t, Date.t, [Accounting.LineItem.t], timeout) :: :ok | {:error, term}
   @callback fetch_account_transactions(String.t, timeout) :: {:ok, [Accounting.AccountTransaction.t]} | {:error, term}
+  @callback receive_money(String.t, Date.t, [Accounting.LineItem.t], timeout) :: :ok | {:error, term}
+  @callback register_categories([atom], timeout) :: :ok | {:error, term}
+  @callback spend_money(String.t, Date.t, [Accounting.LineItem.t], timeout) :: :ok | {:error, term}
+  @callback start_link(opts :: any) :: Supervisor.on_start
 end
