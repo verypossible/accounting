e91c23d9ae825b4f0c0b5951e651247a717da4b0
Daniel Spofford <daniel.spofford@verypossible.com>
2017-12-19 08:57:37 -0600

    Add `refute_account_created/2`
    Why:
    
    * To allow testing that certain accounts were not created.
    
    This change addresses the need by:
    
    * Add `Assertions.refute_account_created/2`

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Accounting.Mixfile do
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       package: package(),
-      version: "0.10.2",
+      version: "0.10.3",
       start_permanent: Mix.env === :prod,
     ]
   end
